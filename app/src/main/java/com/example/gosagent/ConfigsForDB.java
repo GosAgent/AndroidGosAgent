7adce6dd9c08da62f2430b4b54f55b127a2d0a82
GosAgent <91615402+GosAgent@users.noreply.github.com>
2022-07-24 17:42:19 +0700

    Delete app directory

--- app/src/main/java/com/example/gosagent/ConfigsForDB.java
+++
@@ -1,6 +0,0 @@
-package com.example.gosagent;
-
-public class ConfigsForDB {
-    public static String dbHost = "http://cj62918.tmweb.ru/chat.php";
-    public static String tableName = "tablestype";
-}
